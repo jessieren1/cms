b1326cad545aff0a90b72527d0ba38155eb0b2ee
jessieren <jessieren0003@gmail.com>
2022-04-21 20:23:01 +1000

    up date sider

--- components/ManagerSider.tsx
+++ components/ManagerSider.tsx
@@ -14,22 +14,29 @@ export default function ManagerSide() {
   return (
     <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
       <Menu.Item key="1" icon={<PieChartOutlined />}>
-        Option 1
+        Overview
       </Menu.Item>
-      <Menu.Item key="2" icon={<DesktopOutlined />}>
-        Option 2
+
+      <Menu.Item key="2" icon={<PieChartOutlined />}>
+        Class Schedule
       </Menu.Item>
-      <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-        <Menu.Item key="3">Tom</Menu.Item>
-        <Menu.Item key="4">Bill</Menu.Item>
-        <Menu.Item key="5">Alex</Menu.Item>
+
+      <SubMenu key="sub1" icon={<PieChartOutlined />} title="Students">
+        <Menu.Item key="3">Student List</Menu.Item>
       </SubMenu>
-      <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-        <Menu.Item key="6">Team 1</Menu.Item>
-        <Menu.Item key="8">Team 2</Menu.Item>
+
+      <SubMenu key="sub2" icon={<PieChartOutlined />} title="Courses">
+        <Menu.Item key="4">All Courses</Menu.Item>
+        <Menu.Item key="5">Add Courses</Menu.Item>
+        <Menu.Item key="6">Edit Courses</Menu.Item>
       </SubMenu>
-      <Menu.Item key="9" icon={<FileOutlined />}>
-        Files
+
+      <Menu.Item key="7" icon={<FileOutlined />}>
+        Class Schedule
+      </Menu.Item>
+
+      <Menu.Item key="8" icon={<FileOutlined />}>
+        Message
       </Menu.Item>
     </Menu>
   );
