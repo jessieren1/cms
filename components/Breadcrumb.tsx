96ed994c3ef3bc2843efdb45d701b210783c8592
jessieren <jessieren0003@gmail.com>
2022-04-22 17:05:41 +1000

    update breadcrumb

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -1,6 +1,8 @@
 import { Breadcrumb } from 'antd';
 import { useRouter } from 'next/router';
 import React from 'react';
+import 'antd/dist/antd.css';
+import { PageHeader } from 'antd';
 
 export default function AppBreadcrumb() {
   const router = useRouter();
@@ -8,9 +10,20 @@ export default function AppBreadcrumb() {
   const paths = path.split('/').slice(1);
   const root = '/' + paths.slice(0, 2).join('/');
 
-  return (
-    <Breadcrumb style={{ margin: '0 16px', padding: 16 }}>
-      <Breadcrumb.Item> AppBreadcrumb</Breadcrumb.Item>
-    </Breadcrumb>
-  );
+  const routes: { path: string; breadcrumbName: string }[] = [];
+
+  paths.forEach((element, index) => {
+    routes.push({
+      path: element,
+      breadcrumbName: element,
+    });
+  });
+
+  return <PageHeader className="site-page-header" breadcrumb={{ routes }} />;
 }
+
+// return (
+//   <Breadcrumb style={{ margin: '0 16px', padding: 16 }}>
+//     <Breadcrumb.Item> AppBreadcrumb</Breadcrumb.Item>
+//   </Breadcrumb>
+// );
