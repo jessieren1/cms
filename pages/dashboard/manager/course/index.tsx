cab7e8f04760364e6c19cf6ea91486f66f922098
jessieren <jessieren0003@gmail.com>
2022-05-16 22:54:28 +1000

    update

--- pages/dashboard/manager/course/index.tsx
+++ pages/dashboard/manager/course/index.tsx
@@ -4,12 +4,11 @@ import ManagerSider from '../../../../components/Sider/ManagerSider';
 import { useEffect, useState } from 'react';
 import { Course } from '../../../../model/course';
 import { getCourses } from '../../../../lib/services/course-api';
-import { Card, Col, Divider, List, Row, Spin } from 'antd';
+import { Button, Card, Col, Divider, List, Row, Spin } from 'antd';
 import styled from 'styled-components';
 import { HeartFilled, UserOutlined } from '@ant-design/icons';
 import Link from 'next/link';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import Image from 'next/image';
 
 const StyledRow = styled(Row)`
   position: relative;
@@ -106,7 +105,18 @@ export function ScrollMode() {
         <List
           grid={{ gutter: 16, xs: 1, sm: 2, md: 2, lg: 4, xl: 4, xxl: 4 }}
           dataSource={data}
-          renderItem={(item) => <List.Item>{CourseList(item)}</List.Item>}
+          renderItem={(item) => (
+            <List.Item key={item.id}>
+              <CourseList {...item}>
+                <Link
+                  href={`/dashboard/${localStorage.getItem('role')}/courses/${item.id}`}
+                  passHref
+                >
+                  <Button type="primary">Read More</Button>
+                </Link>
+              </CourseList>
+            </List.Item>
+          )}
         />
       </InfiniteScroll>
     </>
